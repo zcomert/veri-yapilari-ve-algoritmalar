f907d2922e2f029402e62f825044de208c2e8bb3
zcomert <zcomert@samsun.edu.tr>
2026-02-23 14:04:07 +0300

    Array has been defined.

---
+++ DataStructures/Array/Array.cs
@@ -0,0 +1,57 @@
+﻿namespace Array;
+using System;
+using System.Collections.Generic;
+
+public class Array<T>
+    where T : IComparable
+{
+    private T[] _items;
+    private int _count;
+
+    public Array(int size = 4)
+    {
+        _items = new T[size];
+        _count = 0;
+    }
+
+    public T this[int index]
+    {
+        get => _items[index];
+        set => _items[index] = value;
+    }
+
+    public int Length => _count;
+
+    public int Capacity => _items.Length;
+
+    public void Add(T item)
+    {
+        if (_count == _items.Length)
+        {
+            // Resize the array
+            T[] newItems = new T[_items.Length * 2];
+            Array.Copy(_items, newItems, _items.Length);
+            _items = newItems;
+        }
+        _items[_count] = item;
+        _count++;
+    }
+
+    public bool Remove(T item)
+    {
+        for (int i = 0; i < _count; i++)
+        {
+            if (EqualityComparer<T>.Default.Equals(_items[i], item))
+            {
+                int elementsToShift = _count - i - 1;
+                if (elementsToShift > 0)
+                {
+                    Array.Copy(_items, i + 1, _items, i, elementsToShift);
+                }
+                _items[--_count] = default!;
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Tests/ArrayTests/ArrayTests.cs
@@ -0,0 +1,107 @@
+using Array;
+
+namespace ArrayTests;
+
+public class ArrayTests
+{
+    [Fact]
+    public void Add_ShouldIncreaseLength_AndStoreItem()
+    {
+        var arr = new Array<int>();
+        arr.Add(10);
+        arr.Add(20);
+
+        Assert.Equal(2, arr.Length);
+        Assert.Equal(10, arr[0]);
+        Assert.Equal(20, arr[1]);
+    }
+
+    [Fact]
+    public void Add_ShouldResize_WhenCapacityExceeded()
+    {
+        var arr = new Array<int>(2);
+        arr.Add(1);
+        arr.Add(2);
+        arr.Add(3); // Should trigger resize
+
+        Assert.Equal(3, arr.Length);
+        Assert.Equal(1, arr[0]);
+        Assert.Equal(2, arr[1]);
+        Assert.Equal(3, arr[2]);
+    }
+
+    [Fact]
+    public void Remove_ShouldRemoveItem_AndShiftElements()
+    {
+        var arr = new Array<int>();
+        arr.Add(1);
+        arr.Add(2);
+        arr.Add(3);
+
+        bool removed = arr.Remove(2);
+
+        Assert.True(removed);
+        Assert.Equal(2, arr.Length);
+        Assert.Equal(1, arr[0]);
+        Assert.Equal(3, arr[1]);
+    }
+
+    [Fact]
+    public void Remove_ShouldReturnFalse_WhenItemNotFound()
+    {
+        var arr = new Array<int>();
+        arr.Add(1);
+        arr.Add(2);
+
+        bool removed = arr.Remove(3);
+
+        Assert.False(removed);
+        Assert.Equal(2, arr.Length);
+    }
+
+    [Fact]
+    public void Indexer_ShouldGetAndSetItems()
+    {
+        var arr = new Array<string>();
+        arr.Add("a");
+        arr.Add("b");
+
+        Assert.Equal("a", arr[0]);
+        Assert.Equal("b", arr[1]);
+
+        arr[1] = "c";
+        Assert.Equal("c", arr[1]);
+    }
+
+    [Fact]
+    public void Length_ShouldReturnNumberOfItems()
+    {
+        var arr = new Array<double>();
+        Assert.Equal(0, arr.Length);
+
+        arr.Add(1.1);
+        arr.Add(2.2);
+
+        Assert.Equal(2, arr.Length);
+    }
+    [Fact]
+    public void Capacity_ShouldReflectInternalArraySize_AndGrowOnResize()
+    {
+        var arr = new Array<int>(2);
+        // Baþlangýç kapasitesi 2 olmalý
+        Assert.Equal(2, arr.Capacity);
+
+        arr.Add(1);
+        arr.Add(2);
+        // Kapasite hala 2 olmalý (henüz resize olmadý)
+        Assert.Equal(2, arr.Capacity);
+
+        arr.Add(3); // Resize tetiklenir (2 -> 4)
+        Assert.Equal(4, arr.Capacity);
+
+        arr.Add(4);
+        arr.Add(5); // Resize tetiklenir (4 -> 8)
+        Assert.Equal(8, arr.Capacity);
+    }
+}
+
