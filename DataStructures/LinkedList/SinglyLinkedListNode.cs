5cafcd750ac8f5637ca87768c7a4f9fb844e6962
zcomert <zafer.comert@hotmail.com>
2026-03-09 23:32:35 +0300

    SinglyLinkedList has been defined.

---
+++ DataStructures/LinkedList/SinglyLinkedListNode.cs
@@ -0,0 +1,24 @@
+﻿namespace LinkedList;
+
+public class SinglyLinkedListNode<T>
+{
+    public T? Item { get; set; }
+    public SinglyLinkedListNode<T> Next { get; set; }
+
+    public SinglyLinkedListNode()
+    {
+
+    }
+    public SinglyLinkedListNode(T? item)
+    {
+        Item = item;
+    }
+
+    public override string ToString()
+    {
+        return Item?.ToString() ?? "null";
+    }
+
+
+
+}
