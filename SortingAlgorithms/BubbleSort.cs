30b3b6abf03f4fceb284abf5f82f910b4afff947
furkancan demircan <furkancan.demircan@gmail.com>
2026-04-17 09:03:48 +0300

    BubbleSort optimized!

--- SortingAlgorithms/BubbleSort.cs
+++ SortingAlgorithms/BubbleSort.cs
@@ -7,8 +7,16 @@ public class BubbleSort
     {
         int n = arr.Length;
         for (int i = 0; i < n - 1; i++)
+        {
+            bool IsSwapped = false;
             for (int j = 0; j < n - i - 1; j++)
                 if (arr[j].CompareTo(arr[j + 1]) >= 1)
+                {
                     Sorting.Swap(arr, j, j + 1);
+                    IsSwapped = true;
+                }
+            if (!IsSwapped)
+                return;
+        }
     }
 }
